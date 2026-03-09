54680106d6a7d3b2b55d0a01efe49bb42f2abf65
Bryan Pinheiro <bryan.superman@hotmail.com>
2026-03-08 22:39:44 -0300

    subindo projeto

---
+++ src/main/java/com/radiadorespinheiro/RadiadoresPinheiroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.radiadorespinheiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadiadoresPinheiroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadiadoresPinheiroBackendApplication.class, args);
+	}
+
+}
