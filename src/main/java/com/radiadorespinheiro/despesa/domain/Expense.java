8f1c6fde0e43f36d29d6c59647ab85dd39779194
luizfernandoreisfranca <luizfernandoreisfranca@gmail.com>
2026-03-13 16:10:04 -0300

    feat: cria módulo de despesas - entidades, repositories e service

--- src/main/java/com/radiadorespinheiro/despesa/domain/Expense.java
+++ src/main/java/com/radiadorespinheiro/despesa/domain/Expense.java
@@ -1,4 +1,39 @@
 package com.radiadorespinheiro.despesa.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "expenses")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal value;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private ExpenseCategory category;
+
+    @Column
+    private String notes;
 }
